3bc0bb1d3ce4da4a49c55d16e8c0d88dfe839b4c
erasebegin <chris.james.haupt@gmail.com>
2020-08-25 20:29:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FaTrash } from "react-icons/fa";
+import { AiOutlinePlusSquare } from "react-icons/ai";
+import "./App.css";
 
 function App() {
+  const [inputString, setInputString] = useState("");
+  const [inputData, setInputData] = useState([]);
+
+  useEffect(() => {
+    if (localStorage.getItem("mydata")) {
+      setInputData(JSON.parse(localStorage.getItem("mydata")));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("mydata", JSON.stringify(inputData));
+  }, [inputData]);
+
+  const getInput = (input) => {
+    input.preventDefault();
+    setInputString(input.target.value);
+  };
+
+  const addItem = () => {
+    setInputData([...inputData, inputString]);
+    setInputString("");
+  };
+
+  const deleteItem = (index) => {
+    let temp = inputData.slice();
+    temp.splice(index, 1);
+    setInputData(temp);
+  };
+
+  const checkKey = (event) => {
+    if (event.key === "Enter") {
+      addItem();
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Jotter</h1>
+        <div class="input-container">
+          <input
+            type="text"
+            onChange={getInput}
+            value={inputString}
+            onKeyPress={checkKey}
+          />
+          <button onClick={addItem}>
+            <AiOutlinePlusSquare />
+          </button>
+        </div>
+        <div className="card-container">
+          {inputData.length > 0
+            ? inputData.map((item) => {
+                return (
+                  <div className="card">
+                    <p>{item}</p>
+                    <button
+                      onClick={() => deleteItem(inputData.indexOf(item))}
+                      onKeyPress={addItem}
+                    >
+                      <FaTrash />
+                    </button>
+                  </div>
+                );
+              })
+            : ""}
+        </div>
       </header>
     </div>
   );
